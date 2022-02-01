1569ced2c90d605222e7357a013eacc17d0f4bf5
kesanakurti yasoda <yashoda2000.k@gmail.com>
2022-02-01 16:44:53 +0530

    completed

---
+++ intro.js
@@ -0,0 +1,81 @@
+//Assignment-1
+var count = 4;
+while (count <= 6) {
+  console.log("Yashoda");
+  count++;
+}
+
+//Assignment-2
+for (var limit = 4; 
+    limit <= 7; limit++) {
+  console.log("yashoda");
+}
+
+//Assignment-3
+var n = 25;
+while (n >= 20) {
+  console.log(n);
+  n--;
+}
+
+//Assignment-4
+for (var n = 13; n >= 0;  n = n - 4) {
+  console.log(n);
+}
+
+//Assignment-5
+var limit = 5;
+var sum = 0;
+for (var k = 1; k <= limit; k++) {
+  sum = sum + k;
+}
+var average = sum / limit;
+console.log(average);
+
+//Assignment-6
+var sum = 0;
+for (var n = 1; n <= 100; n++) {
+  sum = sum + n;
+}
+var average = sum / 100;
+console.log(average);
+
+//Assignment-7
+for (var n = 3; n <= 100; n = n + 3) {
+  console.log(n);
+}
+
+//Assignment-8
+var string = "coding";
+for (var k = 0; k < string.length; k++) {
+  if (
+    string[k] === "c" ||
+    string[k] === "d" ||
+    string[k] === "n" ||
+    string[k] === "g"
+  ) {
+    console.log(string[k]);
+  }
+}
+
+//Assignment-9
+var string = "mathematics";
+console.log(string.length);
+
+//Assignment-10
+var char = "edstem";
+console.log(char[0]);
+console.log(char[5]);
+
+//Assignment-11
+var string = "yashoda";
+for (n = 1; n < string.length; n = n + 2) {
+  console.log(string[n]);
+}
+
+//Assignment-12
+var sum = 0;
+for (var k = 1; k <= 10; k++) {
+  sum = sum + k;
+}
+console.log(sum);
